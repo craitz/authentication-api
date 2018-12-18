06e50751e2c6077fa6bb5e71c5cbdf12d96fe67e
Camilo Raitz da Silva <camilo.rsilva@hsl.org.br>
2018-12-18 15:38:01 -0200

    repositorio inicial

---
+++ src/main/java/org/hsl/adma/authentication/AuthenticationApiApplication.java
@@ -0,0 +1,14 @@
+package org.hsl.adma.authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationApiApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/org/hsl/adma/authentication/AuthenticationApiApplicationTests.java
@@ -0,0 +1,17 @@
+package org.hsl.adma.authentication;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AuthenticationApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
